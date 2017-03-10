26ded66f1d1158e31231f03eb99c4788f1df0408
Johanna Simonsson <johanna.simonsson.18@gmail.com>
2017-03-10 23:03:48 +0100

    Adds root check

--- installation.sh
+++ installation.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
-# Run as root. This script assumes joh-conf/ is placed at $HOME
-
-apt-get -y install emacs24 emacs24-el emacs24-common-non-dfsg terminator texlive autojump tig uswsusp
+apt-get -y install emacs24 emacs24-el emacs24-common-non-dfsg terminator texlive autojump zsh tig
 
 CURRENTPATH=$(dirname "$0")
-echo $CURRENTPATH
 
 # Copy terminator settings
+mkdir -p ~/.config/terminator
 cp $CURRENTPATH/conf/terminator/config ~/.config/terminator/
 cp $CURRENTPATH/conf/init.el ~/.emacs.d/
 
-
-#Set default terminal to terminator
+# Set default terminal to terminator
 gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
 
+# Oh my zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
+
+echo "Reboot to activate zsh"
