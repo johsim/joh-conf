e847250f51f0f6550e384f723ae4d6a6508ac99a
Johanna <johanna.simonsson.18@gmail.com>
2017-11-06 21:14:46 +0100

    Move installation to zplug and ansible

--- conf/.zshrc
+++ conf/.zshrc
@@ -23,6 +23,11 @@ source ~/.zplug/init.zsh
 export FZ_CMD=j
 zplug "changyuheng/fz", defer:1
 zplug "rupa/z", use:z.sh
+zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-autosuggestions", at:master
+zplug "changyuheng/zsh-interactive-cd"
+
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
--- installation.sh
+++ installation.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-# Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-
-# nodejs
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-
-# Emacs
-sudo add-apt-repository -y ppa:kelleyk/emacs
-
-sudo apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade
-
-sudo apt -y install emacs25 terminator \
-     texlive autojump zsh tree python3-dev python3-pip npm python-pygments \
-     google-chrome-stable curl nodejs tig httpie kdiff3 xclip
-
-pip3 install --user thefuck
-sudo pip install virtualenv virtualenvwrapper
-
-sudo npm install -g git-standup
-sudo npm install -g tiny-care-terminal
-
-## fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
-
 CURRENTPATH=$(dirname "$0")
 
 echo -n "Enter host name and press [ENTER]: "
@@ -68,16 +39,6 @@ git config --global --add diff.guitool kdiff3
 git config --global --add difftool.kdiff3.path "$(which kdiff3)"
 git config --global --add difftool.kdiff3.trustExitCode false
 
-# Oh my zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-chsh -s `which zsh`
-
-ZSH_CUSTOM=$CURRENTPATH/conf/zsh_custom
-
-# Zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/changyuheng/zsh-interactive-cd.git $ZSH_CUSTOM/plugins/zsh-interactive-cd
 
 # Link .zshrc
 rm ~/.zshrc
