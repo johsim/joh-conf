39253c0c37d40b322c73f1f9b6f453548a5eee49
Johanna <johanna.simonsson.18@gmail.com>
2017-11-07 00:58:22 +0100

    Set default terminal and outline initial script

--- installation.sh
+++ installation.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-CURRENTPATH=$(dirname "$0")
+# Install ansible
 
-# Set default terminal to terminator
-gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
-
-git config --global core.excludesfile '~/.gitignore_global'
-
-# Git diff to cat
-git config --global core.pager cat
-
-echo -n "Enter email and press [ENTER]: "
-read email
-
-git config --global user.email $email
-
-# Kdiff3 as mergetool
-git config --global --add merge.tool kdiff3
-git config --global --add mergetool.kdiff3.path "$(which kdiff3)"
-git config --global --add mergetool.kdiff3.trustExitCode false
-git config --global mergetool.keepBackup false
-
-git config --global --add diff.guitool kdiff3
-git config --global --add difftool.kdiff3.path "$(which kdiff3)"
-git config --global --add difftool.kdiff3.trustExitCode false
-
-
-# Link .zshrc
-rm ~/.zshrc
-ln -s ~/joh-conf/conf/.zshrc ~/
+# Setup cron job
+# Example: https://github.com/ansible/ansible-examples/blob/master/language_features/ansible_pull.yml
 
 echo "Reboot to activate zsh"
