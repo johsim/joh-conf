1302ae2937a71130e42efe011f4fcdac3b6177cd
Johanna <johanna.simonsson.18@gmail.com>
2017-11-07 00:00:35 +0100

    Configuration for git etc. is done with ansible

--- installation.sh
+++ installation.sh
@@ -2,23 +2,9 @@
 
 CURRENTPATH=$(dirname "$0")
 
-echo -n "Enter host name and press [ENTER]: "
-read hostname
-
-echo $hostname > ~/hostname
-
-# Link terminator settings
-mkdir -p ~/.config/terminator
-ln -s ~/joh-conf/conf/terminator/config ~/.config/terminator/
-
-# Emacs conf.
-cp -p $CURRENTPATH/conf/init.el ~/.emacs.d/
-
 # Set default terminal to terminator
 gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
 
-# Global gitignore
-cp -p $CURRENTPATH/conf/.gitignore_global ~/
 git config --global core.excludesfile '~/.gitignore_global'
 
 # Git diff to cat
