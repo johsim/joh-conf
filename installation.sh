69935a579b0ea569a3729940dcafb32c35274037
Johanna <johanna.simonsson.18@gmail.com>
2017-11-07 17:17:00 +0100

    Outline installation script and fix dependencies for a fresh host

--- installation.sh
+++ installation.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # Install ansible
 
+# To test
+# $ docker run --name ubuntu_xenial -v $(pwd):/workspace -w /workspace -id --rm ubuntu:xenial
+# $ docker exec -it ubuntu_xenial /bin/bash
+
+sudo apt -y update
+sudo apt install -y python3-pip virtualenv
+virtualenv default
+pip3 install ansible
+cd ansible/
+sudo ansible-playbook host-configuration.yaml
+
 # Setup cron job
 # Example: https://github.com/ansible/ansible-examples/blob/master/language_features/ansible_pull.yml
-
-echo "Reboot to activate zsh"
