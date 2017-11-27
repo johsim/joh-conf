d7ed1ccc77ca2c6f5de3b89e0e8c5706e8528eaf
Johanna <johanna.simonsson.18@gmail.com>
2017-11-27 21:03:46 +0100

    Prettify

--- util/cron-ansible.sh
+++ util/cron-ansible.sh
@@ -2,7 +2,7 @@
 echo "Running ansible host configuration..."
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [[ $current_branch == 'ansible' ]]
+if [[ $current_branch == 'master' ]]
 then
     cd ../ansible
     ansible-playbook host-configuration.yaml -e user=$1
