e80adaa158a02f1be2238fca1bb4acc2af7c4039
Johanna <johanna.simonsson.18@gmail.com>
2017-11-27 21:08:50 +0100

    Prettify shell script

--- installation.sh
+++ installation.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-# Install ansible
-
-# To test
-# $ cd ~/joh-conf
-# $ docker build -t ubuntutest:xenial
-# $ docker run -v $(pwd):/home/test/joh-conf --name utest ubuntutest:xenial
-# $ docker exec -it utest /bin/bash
+# Install ansible to run ansible playbook
 
 sudo apt -y update
 sudo apt install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt -y update
 sudo apt install -y ansible
+
 cd ansible/
 ansible-playbook host-configuration.yaml --ask-become-pass
-
-# Setup cron job
-# Example: https://github.com/ansible/ansible-examples/blob/master/language_features/ansible_pull.yml
