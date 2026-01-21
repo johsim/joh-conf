af8ddf748ee62d3a050fcccdb532d63e7bdd2b3a
Johanna Simonsson <johanna.simonsson.18@gmail.com>
2026-01-21 13:55:56 +0100

    update installation script

--- installation.sh
+++ installation.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 # Install ansible to run ansible playbook
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Homebrew is required but not found. Please install Homebrew from https://brew.sh first."
+  exit 1
+fi
 
-sudo apt -y update
-sudo apt install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt -y update
-sudo apt install -y ansible
-
-versions=$(cat ansible/group_vars/all.yaml)
-
-msg="""The current versions will be installed:
-$versions
-Is this correct? (y/n)"""
-
-read -p " > $msg " yn
-case $yn in
-  [Yy]* ) ;;
-  * ) echo "Change versions in ansible/group_vars/all.yaml" && exit 1;;
-esac
+brew update
+brew install ansible
 
 cd ansible/
-python3 $(which ansible-playbook) host-configuration.yaml --ask-become-pass
+ansible-playbook host-configuration.yaml --ask-become-pass
